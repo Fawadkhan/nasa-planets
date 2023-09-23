51fab74aebdc5c07ec05114f43593edbaf6772e8
fawadkhan <fawad.khan@wundermobility.com>
2023-09-23 18:43:59 +0200

    chore: build deployed

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const planetsRouter  = require('./routes/planets/planets.router');
 const cors = require('cors');
 
@@ -8,7 +9,13 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
 app.use(express.json());
-app.use(planetsRouter)
+app.use(express.static(path.join(__dirname, '..', 'public' )));
+app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
