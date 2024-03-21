dd654de1462f4d31fb6157c00f3678a607e0ac0b
fawad khan <fovadkhan@outlook.com>
2024-03-21 10:29:23 +0100

    chore: cors added

--- server/src/app.js
+++ server/src/app.js
@@ -1,31 +1,27 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const morgan = require('morgan');
 
 const planetsRouter  = require('./routes/planets/planets.router');
 const launchesRouter = require('./routes/launches/launches.router');
 
-
-
 const app = express();
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// CORS middleware setup
+app.use(cors({
+    origin: 'https://nasa-client-eight.vercel.app' // Allow requests from this origin
+}));
 
 app.use(morgan('combined'));
-
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public' )));
 app.use(planetsRouter);
 app.use(launchesRouter);
 
+// Handle all other routes by serving the index.html
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
-
-
 
 module.exports = app;
