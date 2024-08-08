a4647931a9ea7da9a6982efedce86552d88ff695
fawad khan <fovadkhan@outlook.com>
2024-08-08 23:59:52 +0200

    chore: update

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const MONGO_URL = `${process.env.MONGODB_URI}` || `${process.env.REACT_APP_MONGODB_URI}`
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGODB_URI || process.env.REACT_APP_MONGODB_URI
 
 console.log("MONGO_URL PROCESS ENV", process.env)
 
